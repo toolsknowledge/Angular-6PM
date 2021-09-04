62f4677d526f4832c00109f84382289cbc2520eb
Sambasivarao <samba.pendela@gmail.com>
2021-09-04 18:14:59 +0530

    interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -3,29 +3,68 @@
 //implementation provided by either json or classes
 
 
+/*
+    interface interface1{
+        sub_one:string;
+        sub_two:string;
+        sub_three:string;
+    };
+
+    let obj1:interface1 = {
+        sub_one : "Angular12",
+        sub_two : "NodeJS",
+        sub_three : "MongoDB"
+    }
+
+    let obj2:interface1 = {
+        sub_one : "VueJS",
+        sub_two : "Go",
+        sub_three : "CassandraDB"
+    };
+    console.log(
+        obj1.sub_one, obj1.sub_two, obj1.sub_three
+    );      //Angular12 NodeJS MongoDB
+    console.log(
+        obj2.sub_one, obj2.sub_two, obj2.sub_three 
+    );      //VueJS Go CassandraDB
+*/
+
+
 interface interface1{
-    sub_one:string;
-    sub_two:string;
-    sub_three:string;
-};
-
-let obj1:interface1 = {
-    sub_one : "Angular12",
-    sub_two : "NodeJS",
-    sub_three : "MongoDB"
+    fun_one():any;
+    fun_two():any;
+    fun_three():any;
 }
-
-let obj2:interface1 = {
-    sub_one : "VueJS",
-    sub_two : "Go",
-    sub_three : "CassandraDB"
+let obj:interface1 = {
+    fun_one : ():any=>{
+        return "Hello_1";
+    },
+    fun_two : ():any=>{
+        return "Hello_2";
+    },
+    fun_three : ():any=>{
+        return "Hello_3";
+    }
 };
 console.log(
-    obj1.sub_one, obj1.sub_two, obj1.sub_three
-);      //Angular12 NodeJS MongoDB
-console.log(
-    obj2.sub_one, obj2.sub_two, obj2.sub_three 
-);      //VueJS Go CassandraDB
+    obj.fun_one(),
+    obj.fun_two(),
+    obj.fun_three()
+);          //Hello_1 Hello_2 Hello_3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
